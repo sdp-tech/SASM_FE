03a919d87f0de13d21e39de53ea5249f42a33591
MinHK4 <dzf4444@gmail.com>
2022-01-18 14:50:45 +0900

    Routing style change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home'
 import Auth from './pages/Auth'
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/auth/*" element={<Auth />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth/*" element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
