4700669670a6e8da60a1eec33c674e3d9ebc51ba
MinHK4 <dzf4444@gmail.com>
2022-05-25 20:03:13 +0900

    API call Login Done

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import { ConstructionOutlined } from '@mui/icons-material';
+import React, { useState } from 'react';
 import CheckLogin from '../../functions/Auth/CheckLogin';
 import { AuthContent, InputWithLabel, AuthButton, RightAlignedLink } from './module';
 
 const Login = () => {
+
+  const [info, setInfo] = useState({});
+
   const props ={
-    email : 'dzf12224@naver.com',
-    password: '4434'
+    email : 'dzf12424@naver.com',
+    password: '4445'
   }
 
   return (
     <AuthContent title="LOG IN">
-        <InputWithLabel name="email" placeholder="Email"/>
-        <InputWithLabel  name="password" placeholder="Password" type="password"/>
+        <InputWithLabel onChange={(event)=>{
+          setInfo({
+            ...info,
+            email: event.target.value
+          })}} 
+          name="email" placeholder="Email"/>
+          
+        <InputWithLabel  onChange={(event)=>{
+          setInfo({
+            ...info,
+            password: event.target.value
+          })}} 
+          name="password" placeholder="Password" type="password"/>
+
         <RightAlignedLink to="/auth/register">아이디/비밀번호 찾기</RightAlignedLink>
         <RightAlignedLink to="/auth/register">회원가입 하기</RightAlignedLink>
-        <AuthButton onClick={()=>CheckLogin(props)}>Log in</AuthButton>
+        <AuthButton onClick={()=>CheckLogin(info)}>Log in</AuthButton>
     </AuthContent> 
   );
 }
