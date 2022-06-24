43b3e016ee1280c85fea0baef21d240518c9173f
MinHK4 <dzf4444@gmail.com>
2022-06-24 16:27:23 +0900

    proxy trying

--- src/functions/Auth/TrySocialLogin.js
+++ src/functions/Auth/TrySocialLogin.js
@@ -5,11 +5,12 @@ export default async function TrySocialLogin(site) {
 
   var url = ''
   if (site === 'google')
-    // url = 'http://127.0.0.1:8000/users/google/login/'
-    url = 'https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?client_id=1037934278190-17b3othcsdmik8im5e1occ4u4kfpko2f.apps.googleusercontent.com&response_type=code&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Fusers%2Fgoogle%2Fcallback%2F&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&flowName=GeneralOAuthFlow'
+    url = '/users/google/login/'
+    // url = '/o/oauth2/v2/auth/oauthchooseaccount?client_id=1037934278190-17b3othcsdmik8im5e1occ4u4kfpko2f.apps.googleusercontent.com&response_type=code&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Fusers%2Fgoogle%2Fcallback%2F&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&flowName=GeneralOAuthFlow'
 
   if (site === 'naver')
-    url = 'naver_url'
+    // url = 'naver_url'
+    url = "http://www.naver.com"
   if (site === 'kakaotalk')
     url = 'kakaotalk_url'
 
