db59f98a5588f7adef6a929bd724033c1a0120d0
mysoo <80390638+mysoo@users.noreply.github.com>
2022-08-27 18:18:57 +0900

    Feat : "detail modal"

--- src/components/SpotMap/SpotDetail.jsx
+++ src/components/SpotMap/SpotDetail.jsx
@@ -3,35 +3,41 @@ import { List } from "@mui/material";
 import DetailCard from "./SpotDetail/DetailCard.js";
 
 export default function SpotDetail(props) {
-  // console.log("hihi", props, props.children);
-  // console.log("hihi2", props.children[0]);
-  // console.log("hihi2", props.children[2]);
+  console.log("hihi", props, props.children);
+  const { modalClose } = props;
 
   return (
     <>
       <List
         style={{ maxHeight: "100%", overflow: "auto", padding: "10px" }}
         sx={{
+          // gridArea: "spotlist",
+
+          boxSizing: "border-box",
           position: "fixed",
-          top: "64px",
-          left: "28%",
+          top: "16.3%",
+          left: "28.7%",
           padding: "0px",
           margin: "0px",
           width: "100%",
-          height: "100%",
+          // height: "100%",
+          height: "82.2%",
           overflow: "hidden",
           maxWidth: 360,
           bgcolor: "#F0F8FF",
-          // border: "1px solid red",
         }}
       >
         <DetailCard
+          ID={props.id}
           ImageURL={props.rep_pic}
           StoreName={props.StoreName}
           StoreType={props.StoreType}
           OpeningHours={props.mon_hours}
           Address={props.Address}
         />
+
+        {/* <CloseButton></CloseButton> */}
+        <button onClick={modalClose}>X</button>
       </List>
     </>
   );
