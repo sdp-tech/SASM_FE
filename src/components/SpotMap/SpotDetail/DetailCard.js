1ec497db54738d317018ad6524599de1f0e6ce09
mysoo <80390638+mysoo@users.noreply.github.com>
2022-08-27 22:51:04 +0900

    Feat : "detail page UI and api url"

--- src/components/SpotMap/SpotDetail/DetailCard.js
+++ src/components/SpotMap/SpotDetail/DetailCard.js
@@ -2,45 +2,172 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledCard = styled.section`
-  // padding: 1em;
+  width: 100%;
 `;
 const ImgBox = styled.div`
   width: 100%;
-  height: 200px;
+  height: 260px;
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 1px solid red;
+  // border: 1px solid red;
+  overflow: hidden;
 `;
-
-const Content = styled.p`
+const TextBox = styled.div`
   font-size: 1em;
   color: black;
+  min-width: 280px;
+  // border: 1px solid yellow;
+  margin: 0.7em 1.4em 0.7em 1.4em;
+  // margin: 5em;
+`;
+const CategoryBox = styled.div`
+  font-size: 1.3em;
+  font-weight: 400;
+  color: black;
+  margin-top: -50px;
+  border-bottom: 1.3px solid #000000;
+  display: flex;
+  flexdirection: column;
+  width: 85%;
+  height: 60px;
+  justify-content: space-between;
+`;
+
+const ReviewBox = styled.div`
+  height: auto;
+  margin-top: 20px;
+  padding: 0.017px;
+  background: #cbced7;
+  font-size: 1em;
+`;
+const AddressBox = styled.div`
+  height: auto;
+  width: 90%;
+  margin-left: 4.5%;
+`;
+
+const PhotoBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  // height: 00px;
+`;
+
+const ShortCurBox = styled.div`
+  height: auto;
+  background: #ffffff;
+`;
+const Title = styled.p`
+  color: #7c1d30;
+  font-size: 2em;
+  margin-top: -2px;
 `;
 
 function DetailCard({
   key,
-  ID,
-  ImageURL,
+  MainImage,
   StoreName,
-  StoreType,
-  OpeningHours,
+  Category,
+  PlaceReview,
+  ShortCur,
   Address,
+  Mon,
+  Tues,
+  Wed,
+  Thurs,
+  Fri,
+  Sat,
+  Sun,
+  Photo0,
+  Photo1,
+  Photo2,
 }) {
   return (
     <StyledCard className="component component--item_card" key={key}>
       <ImgBox>
-        <img src={ImageURL} className="image--itemcard" alt="main image" />
+        <img
+          src={MainImage}
+          className="image--itemcard"
+          alt="main image"
+          width="600px"
+          height="400px"
+        />
       </ImgBox>
-      <Content>{ID}</Content>
-      <Content>{StoreName}</Content>
-      <Content>칙피스</Content>
-      <Content>{StoreType}</Content>
-      <Content>비건식당</Content>
-      <Content>한줄평</Content>
-      <Content>영업시간 : {OpeningHours}</Content>
-      <Content>주소 : {Address}</Content>
-      <Content>신사역 8번 출구에서 465m</Content>
+      <TextBox>
+        <Title>{StoreName}</Title>
+        <CategoryBox>
+          <p>{Category}</p>
+        </CategoryBox>
+        <ReviewBox>
+          {/* PlaceReview */}
+          <p>{PlaceReview}</p>
+        </ReviewBox>
+        <AddressBox>
+          {/* address */}
+          <p
+            style={{
+              fontSize: "1em",
+              fontWeight: "500",
+              color: "black",
+              // marginTop: "-1em",
+            }}
+          >
+            {Address}
+          </p>
+          {/* openingHours */}
+          <div
+            style={{
+              fontSize: "1em",
+              fontWeight: "500",
+              color: "black",
+              height: "100%",
+              // marginTop: "-1em",
+            }}
+          >
+            <p>영업시간 : </p>
+            <p>월 : {Mon}</p>
+            <p>화 : {Tues}</p>
+            <p>수 : {Wed}</p>
+            <p>목 : {Thurs}</p>
+            <p>금 : {Fri}</p>
+            <p>토 : {Sat}</p>
+            <p>일 : {Sun}</p>
+          </div>
+        </AddressBox>
+        <PhotoBox>
+          <img
+            style={{ height: "150px", width: "30%" }}
+            src={Photo0}
+            className="image--itemcard"
+            alt="image1"
+            width="600px"
+            height="400px"
+          />
+          <img
+            style={{ height: "150px", width: "30%" }}
+            src={Photo1}
+            className="image--itemcard"
+            alt="image2"
+            width="600px"
+            height="400px"
+          />
+          <img
+            style={{ height: "150px", width: "30%" }}
+            src={Photo2}
+            className="image--itemcard"
+            alt="image3"
+            width="600px"
+            height="400px"
+          />
+        </PhotoBox>
+        {/* images */}
+        <ShortCurBox>
+          {/* ShortCur */}
+          <p>{ShortCur}</p>
+        </ShortCurBox>
+      </TextBox>
     </StyledCard>
   );
 }
