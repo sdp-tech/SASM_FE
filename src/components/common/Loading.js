c689308bd24294d382393f69cdc00263e5b812be
mysoo <80390638+mysoo@users.noreply.github.com>
2022-08-27 13:47:49 +0900

    Feat : "loading"

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -1,10 +1,31 @@
 import React from "react";
-import { Background, LoadingText } from "./Styles";
+import styled from "styled-components";
+import Spinner from "../../assets/img/Spinner.gif";
+const Background = styled.div`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background: #ffffffb7;
+  z-index: 999;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
-export default () => {
+const LoadingText = styled.div`
+  font: 1rem "Noto Sans KR";
+  text-align: center;
+`;
+
+const Loading = () => {
   return (
     <Background>
-      <LoadingText>잠시만 기다려 주세요.</LoadingText>
+      {/* <LoadingText>잠시만 기다려 주세요.</LoadingText> */}
+       <img src={Spinner} alt="로딩중" width="5%" backgroundColor="none" />
     </Background>
   );
 };
+export default Loading;
