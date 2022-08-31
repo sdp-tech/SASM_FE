5b762a570d0e1856a07e2918a82090bdfbad16b2
mysoo <80390638+mysoo@users.noreply.github.com>
2022-08-31 17:27:27 +0900

    #80 Feat : "No Auth"

--- src/pages/SpotMap.jsx
+++ src/pages/SpotMap.jsx
@@ -27,14 +27,20 @@ export default function SpotMap() {
   });
 
   const loadItem = async () => {
-    console.log(token);
+    let headerValue;
+    if (token === undefined) {
+      headerValue = `No Auth`;
+    } else {
+      headerValue = `Bearer ${token}`;
+    }
+
     setLoading(true);
     try {
       const response = await axios.get(
         "http://127.0.0.1:8000/places/place_list",
         {
           headers: {
-            Authorization: `Bearer ${token}`,
+            Authorization: headerValue,
           },
         }
       );
