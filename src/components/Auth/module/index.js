68ee15757f1036727bce948f5463ae7605652658
mysoo <80390638+mysoo@users.noreply.github.com>
2022-09-01 02:55:11 +0900

    #83 Add : "my page modules"

--- src/components/Auth/module/index.js
+++ src/components/Auth/module/index.js
@@ -1,6 +1,8 @@
-export { default as AuthWrapper } from './AuthWrapper';
-export { default as AuthContent } from './AuthContent';
-export { default as InputWithLabel } from './InputWithLabel';
-export { default as AuthButton } from './AuthButton';
-export { default as RightAlignedLink } from './RightAlignedLink';
-export { default as SocialLogin } from './SocialLogin';
+export { default as AuthWrapper } from "./AuthWrapper";
+export { default as AuthContent } from "./AuthContent";
+export { default as InputWithLabel } from "./InputWithLabel";
+export { default as AuthButton } from "./AuthButton";
+export { default as ProfileButton } from "./ProfileButton";
+export { default as RightAlignedLink } from "./RightAlignedLink";
+export { default as LeftAlignedLink } from "./LeftAlignedLink";
+export { default as SocialLogin } from "./SocialLogin";
