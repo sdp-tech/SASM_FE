ffc6c9b16990e593f77af75a168c029310143512
mysoo <80390638+mysoo@users.noreply.github.com>
2022-09-01 21:10:12 +0900

    #90 Fix : "current location map list"

--- src/pages/SpotMap.jsx
+++ src/pages/SpotMap.jsx
@@ -17,16 +17,46 @@ export default function SpotMap() {
   const [cookies, setCookie, removeCookie] = useCookies(["name"]);
   const token = cookies.name; // 쿠키에서 id 를 꺼내기
 
-  // useEffect(() => {
-  //   setLogin();
-  // }, []);
-
   const [state, setState] = useState({
     loading: false,
     ItemList: [],
   });
 
-  const loadItem = async () => {
+  const [location, setLocation] = useState();
+  // 에러 메세지 저장
+  const [error, setError] = useState();
+
+  // Geolocation의 `getCurrentPosition` 메소드에 대한 성공 callback 핸들러
+  const handleSuccess = (pos) => {
+    const { latitude, longitude } = pos.coords;
+
+    setLocation({
+      latitude,
+      longitude,
+    });
+
+    loadItem(latitude, longitude);
+  };
+
+  // Geolocation의 `getCurrentPosition` 메소드에 대한 실패 callback 핸들러
+  const handleError = (error) => {
+    setError(error.message);
+  };
+
+  useEffect(() => {
+    const { geolocation } = navigator;
+
+    // 사용된 브라우저에서 지리적 위치(Geolocation)가 정의되지 않은 경우 오류로 처리합니다.
+    if (!geolocation) {
+      setError("Geolocation is not supported.");
+      return;
+    }
+
+    // Geolocation API 호출
+    geolocation.getCurrentPosition(handleSuccess, handleError);
+  }, []);
+
+  const loadItem = async (latitude, longitude) => {
     let headerValue;
     if (token === undefined) {
       headerValue = `No Auth`;
@@ -35,9 +65,17 @@ export default function SpotMap() {
     }
 
     setLoading(true);
+
     try {
-      const response = await axios.get(
-        "http://127.0.0.1:8000/places/place_list",
+      const response = await axios.post(
+        "http://127.0.0.1:8000/places/place_list/",
+
+        {
+          //현재 위치 정보 전달
+          left: latitude,
+          right: longitude,
+        },
+
         {
           headers: {
             Authorization: headerValue,
@@ -56,9 +94,9 @@ export default function SpotMap() {
     }
   };
 
-  useEffect(() => {
-    loadItem();
-  }, []);
+  // useEffect(() => {
+  //   loadItem();
+  // }, []);
 
   return (
     <Sections>
