2b7d502c9c3d9f93d8d03225bd20d2c2dbb8bf55
mysoo <80390638+mysoo@users.noreply.github.com>
2022-09-02 01:02:54 +0900

    #84 Fix : "place_list pagination"

--- src/components/SpotMap/SpotList.jsx
+++ src/components/SpotMap/SpotList.jsx
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import styled from "styled-components";
 
 import ItemCard from "./SpotList/ItemCard.js";
 import SearchBar from "./SpotList/SearchBar.js";
 import nothingIcon from "../../assets/img/nothing.svg";
 import Pagination from "../common/Pagination";
+import { useCookies } from "react-cookie";
+import axios from "axios";
+import Loading from "../common/Loading";
+
 const SpotListSection = styled.div`
   // background-color: blue;
   position: relative;
@@ -74,15 +78,19 @@ const SpotsWrapper = styled.div`
 `;
 
 const SpotList = (props) => {
-  const Item = props.Itemcard;
-  console.log("Item", Item);
+  const totalCount = props.Itemcard.count;
+  const Item = props.Itemcard.results;
+  // console.log("Item", Item);
+  const [item, setItem] = useState([]);
   const [searchedItems, setSearchedItems] = useState([...Item]);
-
+  const [cookies, setCookie, removeCookie] = useCookies(["name"]);
   const [filterToggle, setFilterToggle] = useState(false);
   const [searchToggle, setSearchToggle] = useState(false);
   const [limit, setLimit] = useState(20);
   const [page, setPage] = useState(1);
   const offset = (page - 1) * limit;
+  const token = cookies.name; // 쿠키에서 id 를 꺼내기
+  const [loading, setLoading] = useState(true);
 
   const handleFilterToggle = () => {
     setFilterToggle(!filterToggle);
@@ -91,70 +99,112 @@ const SpotList = (props) => {
     setSearchToggle(true);
   };
 
+  //현재 page data 요청
+  const pagePlacelist = async () => {
+    let newPage;
+    if (page == 1) {
+      newPage = null;
+    } else {
+      newPage = page;
+    }
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        "http://127.0.0.1:8000/places/place_list/",
+
+        {
+          params: {
+            page: newPage,
+          },
+        },
+
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      // console.log("data!!@", response);
+      setItem(response.data.results);
+      setLoading(false);
+    } catch (err) {
+      console.log("Error >>", err);
+    }
+  };
+
+  // page가 변경될 때마다 page를 붙여서 api 요청하기
+  useEffect(() => {
+    pagePlacelist();
+  }, [page]);
+
   return (
-    <SpotListSection>
-      <SearchFilterBar>
-        <SearchBar
-          handleFilterToggle={handleFilterToggle}
-          handleSearchToggle={handleSearchToggle}
-        />
-      </SearchFilterBar>
-
-      {filterToggle ? <FilterOptions></FilterOptions> : null}
-
-      {searchToggle ? (
-        <SpotsWrapper>
-          {searchedItems.length === 0 ? (
-            <NothingSearched>
-              <img src={nothingIcon} style={{ marginBottom: "10px" }} />
-              해당하는 장소가 없습니다!
-            </NothingSearched>
-          ) : (
-            searchedItems.map((itemdata, index) => (
-              // 필요 정보 : 이미지,?, 카테고리, ?, 위치, 영업시간
-              <ItemCard
-                key={index}
-                ImageURL={itemdata.rep_pic}
-                StoreName={itemdata.StoreName}
-                StoreType={itemdata.StoreType}
-                OpeningHours={itemdata.OpeningHours}
-                Address={itemdata.Address}
-              />
-            ))
-          )}
-        </SpotsWrapper>
+    <>
+      {loading ? (
+        <Loading />
       ) : (
-        <></>
+        <SpotListSection>
+          <SearchFilterBar>
+            <SearchBar
+              handleFilterToggle={handleFilterToggle}
+              handleSearchToggle={handleSearchToggle}
+            />
+          </SearchFilterBar>
+
+          {filterToggle ? <FilterOptions></FilterOptions> : null}
+
+          {searchToggle ? (
+            <SpotsWrapper>
+              {searchedItems.length === 0 ? (
+                <NothingSearched>
+                  <img src={nothingIcon} style={{ marginBottom: "10px" }} />
+                  해당하는 장소가 없습니다!
+                </NothingSearched>
+              ) : (
+                searchedItems.map((itemdata, index) => (
+                  // 필요 정보 : 이미지,?, 카테고리, ?, 위치, 영업시간
+                  <ItemCard
+                    key={index}
+                    ImageURL={itemdata.rep_pic}
+                    StoreName={itemdata.StoreName}
+                    StoreType={itemdata.StoreType}
+                    OpeningHours={itemdata.OpeningHours}
+                    Address={itemdata.Address}
+                  />
+                ))
+              )}
+            </SpotsWrapper>
+          ) : (
+            <></>
+          )}
+
+          <RecommendTitle>이런 장소는 어떠세요?</RecommendTitle>
+          <SpotsWrapper>
+            {item.map((itemdata, index) => {
+              return (
+                <ItemCard
+                  key={index}
+                  id={itemdata.id}
+                  ImageURL={itemdata.rep_pic}
+                  StoreName={itemdata.place_name}
+                  StoreType={itemdata.category}
+                  open_hours={itemdata.open_hours}
+                  Address={itemdata.address}
+                  place_review={itemdata.place_review}
+                  place_like={itemdata.place_like}
+                />
+              );
+            })}
+
+            <Pagination
+              total={totalCount}
+              limit={limit}
+              page={page}
+              setPage={setPage}
+            />
+          </SpotsWrapper>
+        </SpotListSection>
       )}
-
-      <RecommendTitle>이런 장소는 어떠세요?</RecommendTitle>
-      <SpotsWrapper>
-        {Item &&
-          Item.slice(offset, offset + limit).map((itemdata, index) => {
-            // 필요 정보 : 이미지,?, 카테고리, ?, 위치, 영업시간
-
-            return (
-              <ItemCard
-                key={index}
-                id={itemdata.id}
-                ImageURL={itemdata.rep_pic}
-                StoreName={itemdata.place_name}
-                StoreType={itemdata.category}
-                open_hours={itemdata.open_hours}
-                Address={itemdata.address}
-                place_review={itemdata.place_review}
-                place_like={itemdata.place_like}
-              />
-            );
-          })}
-        <Pagination
-          total={Item.length}
-          limit={limit}
-          page={page}
-          setPage={setPage}
-        />
-      </SpotsWrapper>
-    </SpotListSection>
+    </>
   );
 };
 export default SpotList;
