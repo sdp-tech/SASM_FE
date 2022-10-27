6d215b35d57143fb0ae658ca9a73baa5661e47e8
mysoo <80390638+mysoo@users.noreply.github.com>
2022-10-27 15:02:50 +0900

    #149 Add : "login fail alert & enter submit"

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -43,189 +43,87 @@ const Login = () => {
       break;
     }
   }
+  const LoginClick = async () => {
+    const res = await TryLogin(info);
+
+    if (res === undefined) {
+      alert("로그인 실패");
+    } else if ("success" === res.status) {
+      const access = res.data.access;
+      // alert(access);
+      const refresh = res.data.refresh;
+      // alert(refresh);
+
+      setLogin({
+        ...login,
+        loggedIn: true,
+        // token :res.token
+        refresh: res.data.refresh,
+        access: res.data.access,
+        nickname: res.data.nickname,
+      });
+      localStorage.setItem("nickname", res.data.nickname); //닉네임 따로 저장
+      localStorage.setItem("accessTK", res.data.access); //access token 따로 저장
+
+      // setCookie("name", access);
+      setCookie("name", refresh);
+
+      navigate("/map");
+      // window.location.href = "/map";
+    } else {
+      alert("로그인 실패");
+    }
+  };
+  const onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      LoginClick();
+    }
+  };
 
   return (
     <AuthContent title="LOG IN">
-      <InputWithLabel
-        onChange={(event) => {
-          setInfo({
-            ...info,
-            email: event.target.value,
-          });
-        }}
-        name="email"
-        placeholder="Email"
-        style={flag ? {} : { backgroundColor: "#F9E3E3" }}
-      />
-      <Message>{flag ? "" : "이메일 형식이 올바르지 않습니다"}</Message>
-
-      <InputWithLabel
-        onChange={(event) => {
-          setInfo({
-            ...info,
-            password: event.target.value,
-          });
-        }}
-        name="password"
-        placeholder="Password"
-        type="password"
-      />
-
-      <Message>
-        {!fail
-          ? ""
-          : "SASM에 등록되지 않은 아이디거나, 아이디 또는 비밀번호가 회원정보와 일치하지 않습니다."}
-      </Message>
-
-      <RightAlignedLink to="/auth/find">아이디/비밀번호 찾기</RightAlignedLink>
-      <RightAlignedLink to="/auth/register">회원가입 하기</RightAlignedLink>
-
-      <AuthButton
-        onClick={async () => {
-          const res = await TryLogin(info);
-
-          if ("success" === res.status) {
-            const access = res.data.access;
-            // alert(access);
-            const refresh = res.data.refresh;
-            // alert(refresh);
-
-            setLogin({
-              ...login,
-              loggedIn: true,
-              // token :res.token
-              refresh: res.data.refresh,
-              access: res.data.access,
-              nickname: res.data.nickname,
+      <form onKeyPress={onKeyPress}>
+        <InputWithLabel
+          onChange={(event) => {
+            setInfo({
+              ...info,
+              email: event.target.value,
             });
-            localStorage.setItem("nickname", res.data.nickname); //닉네임 따로 저장
-            localStorage.setItem("accessTK", res.data.access); //access token 따로 저장
+          }}
+          name="email"
+          placeholder="Email"
+          style={flag ? {} : { backgroundColor: "#F9E3E3" }}
+        />
+        <Message>{flag ? "" : "이메일 형식이 올바르지 않습니다"}</Message>
 
-            // setCookie("name", access);
-            setCookie("name", refresh);
+        <InputWithLabel
+          onChange={(event) => {
+            setInfo({
+              ...info,
+              password: event.target.value,
+            });
+          }}
+          name="password"
+          placeholder="Password"
+          type="password"
+        />
 
-            navigate("/map");
-            // window.location.href = "/map";
-          } else {
-            setFail(true);
-          }
-        }}
-      >
-        Log in
-      </AuthButton>
-      <SocialLogin />
+        <Message>
+          {!fail
+            ? ""
+            : "SASM에 등록되지 않은 아이디거나, 아이디 또는 비밀번호가 회원정보와 일치하지 않습니다."}
+        </Message>
+
+        <RightAlignedLink to="/auth/find">
+          아이디/비밀번호 찾기
+        </RightAlignedLink>
+        <RightAlignedLink to="/auth/register">회원가입 하기</RightAlignedLink>
+
+        <AuthButton onClick={LoginClick}>Log in</AuthButton>
+        <SocialLogin />
+      </form>
     </AuthContent>
   );
 };
 
 export default Login;
-
-// import React, { useState, useContext } from "react";
-// import styled from "styled-components";
-// import { LoginContext } from "../../contexts/LoginContexts";
-// import { useNavigate } from "react-router";
-
-// import TryLogin from "../../functions/Auth/TryLogin";
-// import {
-//   AuthContent,
-//   InputWithLabel,
-//   AuthButton,
-//   RightAlignedLink,
-//   SocialLogin,
-// } from "./module";
-
-// const emailFormat = [
-//   "@naver.com",
-//   "@gmail.com",
-//   "@daum.net",
-//   "@hanmail.net",
-//   ".ac.kr",
-// ];
-
-// const Message = styled.div`
-//   font-size: 0.2em;
-//   margin-top: 1.1em;
-//   color: #db524e;
-// `;
-
-// const Login = () => {
-//   const [info, setInfo] = useState({ email: "" });
-//   const [fail, setFail] = useState(false);
-//   const [login, setLogin] = useContext(LoginContext);
-
-//   const navigate = useNavigate();
-
-//   // 이메일 체크
-//   var flag = false;
-//   for (const format of emailFormat) {
-//     if (info.email.includes(format) || info.email === "") {
-//       flag = true;
-//       break;
-//     }
-//   }
-
-//   return (
-//     <AuthContent title="LOG IN">
-//       <InputWithLabel
-//         onChange={(event) => {
-//           setInfo({
-//             ...info,
-//             email: event.target.value,
-//           });
-//         }}
-//         name="email"
-//         placeholder="Email"
-//         style={flag ? {} : { backgroundColor: "#F9E3E3" }}
-//       />
-//       <Message>{flag ? "" : "이메일 형식이 올바르지 않습니다"}</Message>
-
-//       <InputWithLabel
-//         onChange={(event) => {
-//           setInfo({
-//             ...info,
-//             password: event.target.value,
-//           });
-//         }}
-//         name="password"
-//         placeholder="Password"
-//         type="password"
-//       />
-
-//       <Message>
-//         {!fail
-//           ? ""
-//           : "SASM에 등록되지 않은 아이디거나, 아이디 또는 비밀번호가 회원정보와 일치하지 않습니다."}
-//       </Message>
-
-//       <RightAlignedLink to="/auth/find">아이디/비밀번호 찾기</RightAlignedLink>
-//       <RightAlignedLink to="/auth/register">회원가입 하기</RightAlignedLink>
-
-//       <AuthButton
-//         onClick={async () => {
-//           const res = await TryLogin(info);
-//           console.log("외부", res);
-
-//           if ("success" in res) {
-//             setLogin({
-//               ...login,
-//               loggedIn: true,
-//               token: res.token,
-//               nickname: res.nickname,
-//             });
-//             console.log("외부", login);
-
-//             navigate("/map");
-//             // window.location.href = "/map";
-//           } else {
-//             setFail(true);
-//           }
-//         }}
-//       >
-//         Log in
-//       </AuthButton>
-//       <SocialLogin />
-//     </AuthContent>
-//   );
-// };
-
-// export default Login;
