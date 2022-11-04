a0578d31f887ba4e939374be56bee4ad17f96995
Sejong Kim <sepaper@naver.com>
2022-11-04 21:31:32 +0900

    #155 Chore: "change hardcoded url to env var in Auth .js files"

--- src/functions/Auth/TrySocialLogin.js
+++ src/functions/Auth/TrySocialLogin.js
@@ -5,10 +5,10 @@ export default async function TrySocialLogin(site) {
 
   var url = ''
   if (site === 'google')
-    url = 'http://localhost:8000/users/google/login/'
-    // url = '/o/oauth2/v2/auth/oauthchooseaccount?client_id=1037934278190-17b3othcsdmik8im5e1occ4u4kfpko2f.apps.googleusercontent.com&response_type=code&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Fusers%2Fgoogle%2Fcallback%2F&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&flowName=GeneralOAuthFlow'
-    // url = 'https://accounts.google.com'
-    // url = '/'
+    url = process.env.REACT_APP_SASM_API_URL + '/users/google/login/'
+  // url = '/o/oauth2/v2/auth/oauthchooseaccount?client_id=1037934278190-17b3othcsdmik8im5e1occ4u4kfpko2f.apps.googleusercontent.com&response_type=code&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Fusers%2Fgoogle%2Fcallback%2F&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&flowName=GeneralOAuthFlow'
+  // url = 'https://accounts.google.com'
+  // url = '/'
 
   if (site === 'naver')
     // url = 'naver_url'
