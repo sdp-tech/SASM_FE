e58aa70c2a18e117a8acf25e60757b518a9f1597
Rotti_Kim <83882825+KIMROTTI@users.noreply.github.com>
2022-11-24 03:02:25 +0900

    Feature/192
    * #185 feat : social login button
    
    * #192 feat : social login FE2

--- src/functions/Auth/TrySocialLogin.js
+++ src/functions/Auth/TrySocialLogin.js
@@ -1,32 +1,31 @@
 import axios from "axios";
-axios.defaults.withCredentials = true
+axios.defaults.withCredentials = true;
 
 export default async function TrySocialLogin(site) {
-
-  var url = ''
-  if (site === 'google')
-    url = process.env.REACT_APP_SASM_API_URL + '/users/google/login/'
+  var url = "";
+  if (site === "google")
+    url = process.env.REACT_APP_SASM_API_URL + "/users/google/login/";
   // url = '/o/oauth2/v2/auth/oauthchooseaccount?client_id=1037934278190-17b3othcsdmik8im5e1occ4u4kfpko2f.apps.googleusercontent.com&response_type=code&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Fusers%2Fgoogle%2Fcallback%2F&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&flowName=GeneralOAuthFlow'
   // url = 'https://accounts.google.com'
   // url = '/'
 
-  if (site === 'naver')
+  if (site === "naver")
     // url = 'naver_url'
-    url = "http://www.naver.com"
-  if (site === 'kakaotalk')
-    url = 'kakaotalk_url'
+    url = "http://www.naver.com";
+  if (site === "kakaotalk") url = "kakaotalk_url";
 
   console.log(url);
 
-  await axios.get(url, {
-    header: {
-      'Access-Control-Allow-Origin': 'https://accounts.google.com'
-    }
-  })
+  await axios
+    .get(url, {
+      header: {
+        "Access-Control-Allow-Origin": "https://accounts.google.com",
+      },
+    })
     .then(function (res) {
       console.log(res);
     })
     .catch(function (error) {
       console.log(error);
-    })
-};
+    });
+}
