3416bfde4cea5af48ba6983793f961440cbc931b
YoonDH <114418121+dgfh0450@users.noreply.github.com>
2022-12-02 17:19:20 +0900

    #184 feat : place review function applied

--- src/components/SpotMap/SpotDetail/ReviewBox.jsx
+++ src/components/SpotMap/SpotDetail/ReviewBox.jsx
@@ -6,47 +6,43 @@ import Request from '../../../functions/common/Request';
 import WriteReview from '../SpotDetail/WriteReview'
 
 export default function ReviewBox(props) {
-    const [toggle, setToggle] = useState(false);
     const [cookies, setCookie, removeCookie] = useCookies(["name"]);
     const navigate = useNavigate();
     const request = new Request(cookies, localStorage, navigate);
     const reviewInfo = props.data;
     const date = reviewInfo.created.substr(0, 10);
-    const handleToggle = () => {
-        setToggle(!toggle);
+    const setMode = (mode) => {
+        props.setMode(mode);
+    }
+    const setReviewOpen = (mode) => {
+        props.setReviewOpen(mode);
+    }
+    const setTarget = (target) => {
+        props.setTarget(target);
     }
     const reviewDelete = async () => {
-        const response = await request.delete(`/places/place_review/${reviewInfo.id}`, null, null);
+        const response = await request.delete(`/places/place_review/${reviewInfo.id}/`, null, null);
         window.location.reload();
         console.log(response);
     }
-    const reviewUpdate = async (event) => {
-        event.preventDefault();
-        const response = await request.put(`/places/place_review/${reviewInfo.id}`,
-            {
-                place: props.id,
-                contents: event.target.text.value
-            }, null);
-        console.log(response);
-        handleToggle();
-    }
     return (
-        <div style={{padding:'5px', borderBottom:'1px black solid' }}>
+        <div style={{ padding: '5px', borderBottom: '1px black solid' }}>
             <>
-                {reviewInfo.nickname} - {reviewInfo.contents} - {date}
+                {reviewInfo.nickname} - {reviewInfo.contents} - {reviewInfo.id}
+                <div style={{ display: 'flex', border: "1px black solid", justifyContent: 'space-around' }}>
+                    <img src={reviewInfo.photos[0]?.imgfile} style={{ width: '50px' }}></img>
+                    <img src={reviewInfo.photos[1]?.imgfile} style={{ width: '50px' }}></img>
+                    <img src={reviewInfo.photos[2]?.imgfile} style={{ width: '50px' }}></img>
+                </div>
             </>
             {props.writer ?
                 <>
                     <button onClick={reviewDelete}>삭제</button>
-                    
-                    <>
-                        {toggle ? <form onSubmit={(event) => {
-                            reviewUpdate(event)
-                        }}>
-                            <textarea id='text' defaultValue={reviewInfo.contents}></textarea>
-                            <button type='submit'>수정</button>
-                        </form> : <button onClick={handleToggle}>수정</button>}
-                    </>
+                    <button onClick={()=>{
+                        setMode('update');
+                        setReviewOpen(true);
+                        setTarget(reviewInfo.id);
+                    }}>수정</button>
                 </> : null}
         </div>
     )
