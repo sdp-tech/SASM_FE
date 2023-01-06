0dc640d3d60e4fbe25752751b7e57e763cdba56f
YoonDH <114418121+dgfh0450@users.noreply.github.com>
2023-01-06 15:16:54 +0900

    #237 feat : story comment style

--- src/components/Story/components/Comment.jsx
+++ src/components/Story/components/Comment.jsx
@@ -4,41 +4,47 @@ import { useCookies } from 'react-cookie';
 import Request from '../../../functions/common/Request';
 import { useNavigate } from 'react-router-dom';
 
-const CommentWrapper = styled.div`
+const CommentBox = styled.div`
+  margin-bottom: 3vh;
+`
+const InfoBox = styled.div`
+  width: 100%;
+  display: flex;
   position: relative;
-  display:flex;
-  flex-flow: row no wrap;
-  border-bottom: 1px black solid;
+  align-items: center;
+  margin-bottom: 1vh;
 `
-const ButtonWrapper = styled.div`
-  position: absolute;
-  height: 100%;
-  right: 0;
-  display:flex;
-`
-const Button = styled.div`
+const UserBox = styled.div`
   display: flex;
   align-items: center;
-  height: 100%;
-  cursor: pointer;
-  padding: 0 5px;
-  & + & {
-    border-left: 1px black solid;
-  }
-`
-const NickNameBox = styled.div`
-  width:20%;
-  padding: 1% 3%;
-  border-right: 1px black solid;
-  @media screen and (max-width: 768px) {
-    width: 30%;
-  }
+  margin-right: 20px;
+  font-size: 1.25rem;
+  font-weight: 700;
 `
 const ContentBox = styled.div`
-  padding: 1% 3%;
+  width: 100%;
+  font-size: 1rem;
+  font-weight: 500;
+  padding-left: 65px;
 `
-
-export default function Comment({ data, setMode,setTarget }) {
+const ButtonBox = styled.div`
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  right: 0;
+  width: 12%;
+  box-sizing: border-box;
+`
+const Button = styled.button`
+  border: none;
+  outline: none;
+  background-color: white;
+  height: 100%;
+  font-size: 1rem;
+  font-weight: 600;
+`
+export default function Comment({ data, setMode, setTarget }) {
+  const date = data.updated_at.slice(0,10);
   const [cookies, setCookie, removeCookie] = useCookies(["name"]);
   // const token = cookies.name; // 쿠키에서 id 를 꺼내기
   const token = localStorage.getItem("accessTK"); //localStorage에서 accesstoken꺼내기
@@ -56,21 +62,25 @@ export default function Comment({ data, setMode,setTarget }) {
     isWriter = true;
   }
   return (
-    <CommentWrapper>
-      <NickNameBox>
-        {data.nickname}
-      </NickNameBox>
-      <ContentBox>
-        {data.content}
-      </ContentBox>
-      {isWriter ?
-        <ButtonWrapper>
-          <Button onClick={deleteComment}>삭제</Button>
-          <Button onClick={()=>{
-            setMode('update');
-            setTarget(data);
-          }}>수정</Button>
-        </ButtonWrapper> : null}
-    </CommentWrapper>
+    <CommentBox>
+      <InfoBox>
+        <UserBox>
+          <img src={data.profile_image} style={{width: '45px', height:'45px', borderRadius:'50%', marginRight:'20px'}} />
+          {data.nickname}
+        </UserBox>
+        {date}
+        <ButtonBox>
+          {isWriter ?
+            <>
+              <Button onClick={() => {
+                setMode('update');
+                setTarget(data);
+              }}>수정</Button>
+              <Button onClick={deleteComment}>삭제</Button>
+            </> : null}
+        </ButtonBox>
+      </InfoBox>
+      <ContentBox>{data.content}</ContentBox>
+    </CommentBox>
   )
 }
