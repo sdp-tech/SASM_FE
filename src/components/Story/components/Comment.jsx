7e314aaf93df8d1c25e0a488f47c01ec9acaa3fe
YoonDH <114418121+dgfh0450@users.noreply.github.com>
2023-01-06 16:17:25 +0900

    #222 feat : story comment update add
    * #222 feat : story comment update

--- src/components/Story/components/Comment.jsx
+++ src/components/Story/components/Comment.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components';
 import { useCookies } from 'react-cookie';
 import Request from '../../../functions/common/Request';
@@ -36,6 +36,7 @@ const ButtonBox = styled.div`
   box-sizing: border-box;
 `
 const Button = styled.button`
+  cursor: pointer;
   border: none;
   outline: none;
   background-color: white;
@@ -43,20 +44,54 @@ const Button = styled.button`
   font-size: 1rem;
   font-weight: 600;
 `
-export default function Comment({ data, setMode, setTarget }) {
-  const date = data.updated_at.slice(0,10);
+const TextArea = styled.textarea`
+    display:block;
+    margin:0;
+    width: 100%;
+    margin-right: 3vw;
+    height: 5vh;
+    resize:none;
+    border: 1px rgba(0,0,0,0.3) solid;
+    padding: 11px 30px;
+    border-radius:1000px;
+    ::placeholder,
+    ::-webkit-input-placeholder {
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+        left: 5%;
+    }
+    :-ms-input-placeholder {
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+        left: 5%;
+    }
+`
+export default function Comment({ data }) {
+  const date = data.updated_at.slice(0, 10);
+  const [update, setUpdate] = useState(false);
   const [cookies, setCookie, removeCookie] = useCookies(["name"]);
   // const token = cookies.name; // 쿠키에서 id 를 꺼내기
   const token = localStorage.getItem("accessTK"); //localStorage에서 accesstoken꺼내기
   const navigate = useNavigate();
   const request = new Request(cookies, localStorage, navigate);
   const email = localStorage.getItem('email');
+  const [updatetext, setUpdateText] = useState(data.content);
 
+  const handleUpdate = () => {
+    setUpdate(!update);
+  }
   const deleteComment = async () => {
     const response = await request.delete(`/stories/comments/${data.id}`, {});
     window.location.reload();
   }
-
+  const updateComment = async () => {
+    const response = await request.patch(`/stories/comments/${data.id}`, {
+      content: document.getElementById('textarea').value,
+    });
+    window.location.reload();
+  }
   let isWriter = false;
   if (data.email == email) {
     isWriter = true;
@@ -65,22 +100,32 @@ export default function Comment({ data, setMode, setTarget }) {
     <CommentBox>
       <InfoBox>
         <UserBox>
-          <img src={data.profile_image} style={{width: '45px', height:'45px', borderRadius:'50%', marginRight:'20px'}} />
+          <img src={data.profile_image} style={{ width: '45px', height: '45px', borderRadius: '50%', marginRight: '20px' }} />
           {data.nickname}
         </UserBox>
         {date}
         <ButtonBox>
           {isWriter ?
             <>
-              <Button onClick={() => {
-                setMode('update');
-                setTarget(data);
-              }}>수정</Button>
-              <Button onClick={deleteComment}>삭제</Button>
+              {update ?
+                <>
+                  <Button onClick={updateComment}>저장</Button>
+                  <Button onClick={handleUpdate}>취소</Button>
+                </> :
+                <>
+                  <Button onClick={() => {
+                    handleUpdate();
+                  }}>수정</Button>
+                  <Button onClick={deleteComment}>삭제</Button>
+                </>}
             </> : null}
         </ButtonBox>
       </InfoBox>
-      <ContentBox>{data.content}</ContentBox>
+      <ContentBox>
+        {update ? <><TextArea autoFocus id="textarea" value={updatetext} onChange={(event)=>{
+          setUpdateText(event.target.value);
+        }}></TextArea></> : <>{data.content}</>}
+      </ContentBox>
     </CommentBox>
   )
 }
