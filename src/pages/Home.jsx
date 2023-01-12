f6897f9d78065aa42ba064102a6d97aee7bacaf9
lina1919 <lina1919@naver.com>
2023-01-12 11:08:10 +0900

    style : "add main page css"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,58 +1,72 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
-import Button from "@mui/material/Button";
-import logo from "../assets/img/sasm_logo.png";
-import picture from "../assets/img/Main/Picture.png";
-import background1 from "../assets/img/Main/Background1.svg";
-import background2 from "../assets/img/Main/Background2.svg";
-import background3 from "../assets/img/Main/Background3.svg";
-import mapimage from "../assets/img/Main/MapImage.svg";
-import Navibar from "../components/common/Navibar";
+import picture from "../assets/img/Main/LandingPage.png";
+import map from "../assets/img/Main/map_image.png";
+import map_category from "../assets/img/Main/map_component_img.png";
+import background_green from "../assets/img/Main/BackgroundGreen.png";
 import { Pc, Tablet, Mobile } from "../device"
-const ImageBox = styled.div`
-  
-`
+
 const IntroImg = styled.img`
-  width: 100vw;
-  height: auto;
+  float: right;
+  position: absolute;
+  left: 62%;
   @media screen and (max-width: 768px) {
-    height: 30vw;
-  }
-`;
-const HomeImg = styled.img`
-  width: 100vw;
-  height: auto;
+    width: 50%;
+    left: 50%;
+  } 
+  @media screen and (min-width: 769px) and (max-width: 1023px) {
+    width: 50%;
+  } 
 `;
 
 //SASM
 const SASM = styled.div`
   width: 60%;
   height: 550px;
-  margin: 5% auto;
+  margin-left: 10%;
+  margin-top: 10%;
   color: black;
   display: block;
   @media screen and (max-width: 768px) {
-    height: 300px;
+    height: 180px;
+    width: 30%;
   } 
 `;
-const SubTitle = styled.div`
-  font-size: 2vw;
-  font-weight: 600;
+const SASMDescription = styled.div`
+  display: flex;
+  @media screen and (max-width: 768px) {
+    width: 63%;
+  } 
+`
+const SubLogo = styled.div`
+  margin-left: 5%;
   @media screen and (min-width: 769px) and (max-width: 1023px) {
-    font-size: 3vw;
+    font-size: 2vw;
   } 
   @media screen and (max-width: 768px) {
-    font-size: 4vw;
+    font-size: 2vw;
+  }
+`;
+const SubTitle = styled.div`
+  margin-left: 10%;
+  font-size: 1.5vw;
+  @media screen and (min-width: 769px) and (max-width: 1023px) {
+    margin-left: 10%;
+    font-size: 2vw;
+    width: 30%;
+  } 
+  @media screen and (max-width: 768px) {
+    font-size: 2vw;
   }
 `
 const TitleBox = styled.div`
   display: flex;
+  margin-bottom : 7%;
 `
 const Tittle = styled.div`
   box-sizing: border-box;
   color: black;
-  font-size: 7vw;
+  font-size: 10vw;
   font-weight: 700;
   @media screen and (min-width: 769px) and (max-width: 1023px) {
     font-size: 9vw;
@@ -64,69 +78,127 @@ const Tittle = styled.div`
 
 const Content = styled.div`
   box-sizing: border-box;
-  position: absolute;
-  font-size: 1.5vw;
+  font-size: 2vw;
   @media screen and (min-width: 769px) and (max-width: 1023px) {
     font-size: 3vw;
   } 
   @media screen and (max-width: 768px) {
-    font-size: 3.5vw;
+    font-size: 1.5vw;
   } 
 `;
+const ContentBox = styled.div`
+  margin-top: 30vh;
+  margin-left: 5vw;
+  font-size: 2vw;
+  @media screen and (min-width: 769px) and (max-width: 1023px) {
+    font-size: 3vw;
+  } 
+  @media screen and (max-width: 768px) {
+    margin-top: 10vh;
+    font-size: 3.5vw;
+  } 
+`
 //Map
 const Map = styled.div`
-  
-`
-const Background2Box = styled.div`
-  float: right;
-  position: relative;
-  z-index: 2;
-  bottom: 80px;
-`
-const Background1Box = styled.div`
-  position: relative;
-  bottom: 65vw;
-  z-index: 1;
-  @media screen and (max-width: 768px) {
-    bottom: 140vw;
-  }
+  margin-top: 2.5vw;
+  height: 5vw;
+  font-size: 1.5vw;
+  border-bottom:1px solid rgba(0, 0, 0, .5);
   @media screen and (min-width: 769px) and (max-width: 1023px) {
-    bottom: 120vw;
+    font-size: 3vw;
+  } 
+  @media screen and (max-width: 768px) {
+    font-size: 4vw;
+    height: 8vw;
   }
+`
+const MapBox = styled.div`
+  padding-top: auto;
+  padding-bottom: auto;
+  line-height: 3vw;
+  text-align: center;
+  margin-left: 20%;
+  width: 15%;
+  height: 3vw;
+  background: linear-gradient(102.78deg, rgba(255, 255, 255, 0.7) 4.48%, rgba(255, 255, 255, 0) 90.51%), #3AE894;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 75px;
+  @media screen and (max-width: 768px) {
+    width: 20%;
+    height: 5vw;
+    line-height: 5vw;
+  } 
+  @media screen and (min-width: 769px) and (max-width: 1023px) {
+    width: 20%;
+  } 
+`
+const MapDescrip = styled.div`
+  margin-left: 2vw;
+  @media screen and (max-width: 768px) {
+    margin-left: 4vw;
+    font-size: 1vw;
+  } 
+  @media screen and (min-width: 769px) and (max-width: 1023px) {
+    width: 50%;
+  } 
+`
+const MapWrapper = styled.div`
+  display: flex;
+  height: 4vw;
+  line-height: 4vw;
+  @media screen and (max-width: 768px) {
+    height: 6vw;
+    line-height:6vw;
+  } 
+  @media screen and (min-width: 769px) and (max-width: 1023px) {
+    width: 50%;
+  } 
+`
+const MyPickBox = styled.div`
+  padding-top: auto;
+  padding-bottom: auto;
+  line-height: 3vw;
+  text-align: center;
+  margin-left: 20%;
+  width: 15%;
+  height: 3vw;
+  background : linear-gradient(102.78deg, rgba(255, 255, 255, 0.7) 4.48%, rgba(255, 255, 255, 0) 90.51%), #FFE9D0;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 75px;
+  @media screen and (max-width: 768px) {
+    width: 20%;
+    height: 5vw;
+    line-height: 5vw;
+  } 
+  @media screen and (min-width: 769px) and (max-width: 1023px) {
+    width: 20%;
+  } 
 `
 
 const MapSubtitle = styled.div`
-  font-size: 3.5vw;
-  font-weight: 700;
   text-align: center;
-  margin-bottom: 3%;
+  font-size: 1.5vw;
+  border-right: 4px solid #FFFFFF;
   @media screen and (min-width: 769px) and (max-width: 1023px) {
     font-size: 3vw;
   } 
   @media screen and (max-width: 768px) {
     font-size: 4vw;
   }
+  width: 20%;
+  // height: 30%;
 `
 const MapTitle = styled.div`
-  font-size: 5vw;
-  font-weight: 700;
-  text-align: center;
-  letter-spacing: 0.1em;
-  @media screen and (min-width: 769px) and (max-width: 1023px) {
-    font-size: 7vw;
-  } 
-  @media screen and (max-width: 768px) {
-    font-size: 9vw;
-  } 
+  color: #FFFFFF;
+  background-image: url(${background_green});
+  width: 100%;
+  height: auto;
+  background-size: 100% 100%;
 `
-const MapImage = styled.div`
-  position: relative;
-  z-index: 3;
-  // top: 25vh;
-  left: 5vw;
-  width: 95vw;
+const MapImage = styled.img`
+  // width: 80%;
   @media screen and (max-width: 768px) {
-    bottom: 110vw;=
+    bottom: 110vw;
     height: auto;
   }
   @media screen and (min-width: 769px) and (max-width: 1023px) {
@@ -134,203 +206,130 @@ const MapImage = styled.div`
     height: auto;
   }
 `
-const MapImageBox = styled.img`
-  // box-shadow: 0px 12px 32px 4px rgba(0, 0, 0, 0.2);
-  left: 3%;
-  width: 50%;
-  height: auto;
+const CategoryImage = styled.img`
+  @media screen and (max-width: 768px) {
+      bottom: 110vw;
+      height: auto;
+    }
+    @media screen and (min-width: 769px) and (max-width: 1023px) {
+      bottom: 80vw;
+      height: auto;
+    }
 `
+
 const MapWrite = styled.div`
+  font-size: 1.5vw;
 `
-const Wrapper = styled.div`
-  position: absolute;
-  margin: 0 auto;
-  top: 26%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 100%;
-  height: 10%;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  // border: 1px solid RED;
-`;
-//Story
-const Story = styled.div`
 
-`
-const Background3Box = styled.div`
-  position: relative;
-  float: right;
-  bottom: 60vw;
-  @media screen and (max-width: 768px) {
-    bottom: 140vw;
-  }
-  @media screen and (min-width: 769px) and (max-width: 1023px) {
-    bottom: 120vw;
-  }
-`
-const StorySubtitle = styled.div`
-  font-size: 3.5vw;
-  font-weight: 700;
-  margin-bottom: 3%;
-  margin-left: 5%;
-  @media screen and (min-width: 769px) and (max-width: 1023px) {
-    position: relative;
-    font-size: 3vw;
-    bottom: 35vw;
-  } 
-  @media screen and (max-width: 768px) {
-    position: relative;
-    font-size: 4vw;
-    bottom: 50vw;
-  }
-`
-const StoryTitle = styled.div`
-  font-size: 5vw;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  margin-left: 5%;
-  @media screen and (min-width: 769px) and (max-width: 1023px) {
-    position: relative;
-    font-size: 7vw;
-    bottom: 35vw;
-  } 
-  @media screen and (max-width: 768px) {
-    position: relative;
-    font-size: 9vw;
-    bottom: 50vw;
-  } 
-`
-const StoryExample = styled.div`
-  width: 90vw;
-  height: 600px;
-  background-color: rgba(255,255,255,0.8);
-  opacity: 0.8;
-  box-shadow: 0px 4px 7px rgba(0, 0, 0, 0.25);
-  border-radius: 54px;
-  border: 1px solid rgba(0, 0, 0, 0.25);
-  margin: 0 auto;
-`
-const NavBtn = styled.button`
-  background: none;
-  padding: 3vw;
+const StoryWrapper = styled.div`
+  margin-left: 8%;
   display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  border: none;
-  align-items: center;
-  // border: 1px solid yellow;
-`;
-
-const StyledLink = styled(Link)`
-  box-sizing: border-box;
-  display: block;
-  margin: 0 auto;
+  height: 4vw;
+  line-height: 4vw;
+  @media screen and (max-width: 768px) {
+    height: 6vw;
+    line-height:6vw;
+  } 
+  @media screen and (min-width: 769px) and (max-width: 1023px) {
+    width: 50%;
+  } 
+`
+const StoryBox = styled.div`
+  padding-top: auto;
+  padding-bottom: auto;
+  line-height: 3vw;
   text-align: center;
-  font-style: normal;
-  font-weight: 40%;
-  // font-size: 3.5vw;
-  // font-size: 30px;
-  // font-size: 5vw;
-  // font-size: 2.8em;
-  font-size: 2rem;
-  line-height: 35px;
-  color: black;
-  text-decoration: none;
-  // border: 1px solid RED;
-`;
-
-// logo
-const LogoBox = styled.div`
-  // position: absolute;
-  height: 15%;
+  margin-left: 20%;
   width: 15%;
-  margin-left: 10%;
-  margin-top: 1%;
-
-`;
-const Logo = styled.img`
-  display: block;
-  height: 100%;
-  width: 100%;
-  cursor: pointer;
-  display: flex;
-`;
+  height: 3vw;
+  background: linear-gradient(102.78deg, rgba(255, 255, 255, 0.7) 4.48%, rgba(255, 255, 255, 0) 90.51%), #01A0FC;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 75px;
+  @media screen and (max-width: 768px) {
+    width: 20%;
+    height: 5vw;
+    line-height: 5vw;
+  } 
+  @media screen and (min-width: 769px) and (max-width: 1023px) {
+    width: 20%;
+  } 
+`
 //
 export default function Home() {
   return (
     <>
-
-      <ImageBox>
-        <IntroImg src={picture}></IntroImg>
-      </ImageBox>
+      <IntroImg src={picture}></IntroImg>
       <SASM>
-        <SubTitle>Sustainable Act Space Map</SubTitle>
         <TitleBox>
           <Tittle>SASM</Tittle>
-          <LogoBox>
-            <Logo
-              src={logo}
-            // onClick={() => PageRedirection(navigate, "SASM")}
-            ></Logo>
-          </LogoBox>
         </TitleBox>
         <Content>
-          SASM은 지속가능한 공간을 큐레이팅하여, <br /> 한국 도시의 지속가능한
+          SASM은 지속가능한 공간을 큐레이팅하여, <br />한국 도시의 지속가능한
           발전을 돕습니다.
         </Content>
       </SASM>
-      <Map>
-        <MapSubtitle>Subtitle about map</MapSubtitle>
-        <MapTitle>MAP</MapTitle>
-        <Background2Box>
-          <Pc><img src={background2}></img></Pc>
-          <Tablet><img src={background2}></img></Tablet>
-          <Mobile><HomeImg src={background2}></HomeImg></Mobile>
-        </Background2Box>
-        <MapImage>
-          <MapImageBox src={mapimage}></MapImageBox>
-          <MapWrite>방문하고 싶은 장소를 검색하세요. <br />
-            식당, 복합문화공간 등 장소 유형을 선택하거나 <br />
-            지역이나 장소 이름을 검색해도 좋습니다.</MapWrite>
-        </MapImage>
-        <Background1Box>
-          <Pc><img src={background1}></img></Pc>
-          <Tablet><img src={background1}></img></Tablet>
-          <Mobile><HomeImg src={background1}></HomeImg></Mobile>
-        </Background1Box>
-      </Map>
-      <Story>
-        <StorySubtitle>Subtitle about Story</StorySubtitle>
-        <StoryTitle>Story</StoryTitle>
-        <Background3Box>
-          <Pc><img src={background3}></img></Pc>
-          <Tablet><HomeImg src={background3}></HomeImg></Tablet>
-          <Mobile><HomeImg src={background3}></HomeImg></Mobile>
-        </Background3Box>
-        <StoryExample>
-        </StoryExample>
-      </Story>
-
-
-      {/* <Wrapper>
-        {/* hover 추가하기 */}
-      {/* <NavBtn>
-        <StyledLink to="/map">MAP</StyledLink>
-      </NavBtn>
-      <NavBtn>
-        <StyledLink to="/story">STORY</StyledLink>
-      </NavBtn>
-      <NavBtn>
-        <StyledLink to="/mypage">MY PAGE</StyledLink>
-      </NavBtn> */}
-
-      {/* <NavBtn>
-          <StyledLink to="/auth">Sign in</StyledLink>
-        </NavBtn> */}
-      {/* </Wrapper> */}
+      <SASMDescription>
+        <SubLogo>
+            SUSTAINABLE<br/>
+            ACT<br/>
+            SPACE<br/>
+            MAP<br/>
+        </SubLogo>
+        <SubTitle>
+            SASM은 SDGs 목표 실현에 기여하는 여가 공간을<br />
+            '지속가능한 공간'으로 정의하고,<br/>
+            이에 대한 지도화 및 큐레이션을 제공합니다. <br/><br/>
+            '음식점 및 카페, 녹색공간, 제로웨이스트샵, 전시 및 체험공간,<br/>
+            복합문화공간, 도시재생 및 친환경 건축물'<br/><br/>
+            6개의 테마로 분류하여, 다양한 지속가능한 공간을 연결해드립니다.<br/><br/>
+            <b>SASM은 이를 통해 지역 커뮤니티를 활성화하고,<br/>
+            건강한 생산과 소비 활동을 촉진시켜 일상의 지속가능화를 지향합니다.</b>
+        </SubTitle>
+        </SASMDescription>
+        
+        <ContentBox>
+            <b>SASM</b>은 공간 큐레이션 플랫폼으로, <br />
+            크게 <b>세 가지 기능</b>을 제공하고 있습니다.
+        </ContentBox>
+        <br/>
+        <Map>
+          <MapWrapper>
+            <MapBox>Map</MapBox>
+            <MapDescrip>지속가능한 공간을 탐색하고 방문해보세요.</MapDescrip>
+          </MapWrapper>
+        </Map>
+        <Map>
+          <StoryWrapper>
+            <StoryBox>Story</StoryBox>
+            <MapDescrip>공간에 대한 자세한 정보와 인사이트를 얻어가세요.</MapDescrip>
+          </StoryWrapper>
+        </Map>
+        <Map>
+          <MapWrapper>
+            <MyPickBox>My Pick</MyPickBox>
+            <MapDescrip>마음에 드는 공간들의 컬렉션을 만들 수 있어요.</MapDescrip>
+          </MapWrapper>
+        </Map>
+        <br/><br/>
+        <MapTitle>
+          <div style={{display:"flex"}}>
+            <MapSubtitle>
+              SASM 아니면 몰랐을<b>지도</b><br/>
+              이렇게나 많은 지속가능성
+            </MapSubtitle>
+            <MapWrite>
+              <p style={{fontSize:"4vw"}}>MAP</p>
+              <b>식당/카페, 전시/체험, 제로웨이스트<br/>
+              건축물, 복합문화, 녹색공간<br/></b>
+              <br/>
+              SASM은 총 여섯 종류의 지속가능한 공간을 소개합니다. <br/>
+              필터를 선택해서 원하는 유형의 공간을 찾거나, 지역이나 장소를 검색할 수 있어요.<br/>
+              하트를 클릭하면 My Pick에 해당 공간을 저장할 수 있습니다.<br/>
+            </MapWrite>
+            </div>
+            <MapImage src={map}></MapImage>
+            <CategoryImage src={map_category}></CategoryImage>
+        </MapTitle>
     </>
   );
 }
