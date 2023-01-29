311f41b2bc39ea5b16b2e8dc335febd1fbda4dcc
YoonDH <114418121+dgfh0450@users.noreply.github.com>
2023-01-29 15:46:37 +0900

    #330 feat : Community hashtag list and search
    #330 feat : Community hashtag list and search

--- src/components/Community/Contents/PromotionBoard/PromotionBoard.jsx
+++ src/components/Community/Contents/PromotionBoard/PromotionBoard.jsx
@@ -15,8 +15,18 @@ import Pagination from '../../../common/Pagination'
 const Contents = styled.div`
 `
 const SearchFilterBar = styled.div`
+  position: relative;
   width: 60%;
   margin: 5vh auto;
+`
+const HashtagListWrapper = styled.div`
+  position: absolute;
+  width: 100%;
+`
+const HashtagList = styled.div`
+  background-color: rgba(0,0,0,0.5);
+  color: #FFFFFF;
+  padding: 5%;
 `
 export default function PromotionBoard() {
   const [loading, setLoading] = useState(false);
@@ -25,13 +35,20 @@ export default function PromotionBoard() {
   const [cookies, setCookie, removeCookie] = useCookies(["name"]);
   const request = new Request(cookies, localStorage, navigate);
   const [list, setList] = useState([]);
-  const [detail, setDetail] = useState([{}]);
+  const [listHashtag, setListHashtag] = useState([]);
   const [page, setPage] = useState(1);
   const [total, setTotal] = useState(0);
   const [search, setSearch] = useState('');
   const [tempSearch, setTempSearch] = useState('');
   const onChangeSearch = (e) => {
-    e.preventDefault();
+    if (e.target.value[0] == '#') {
+      if (e.target.value.slice(1).length == 0) {
+        setListHashtag([]);
+      }
+      else {
+        getHashTag(e.target.value.slice(1));
+      }
+    }
     setTempSearch(e.target.value);
   };
   const handleSearchToggle = async (e) => {
@@ -49,21 +66,40 @@ export default function PromotionBoard() {
   const handleMode = () => {
     setMode(!mode);
   }
-
+  const getHashTag = async (search) => {
+    const response = await request.get("/community/post_hashtags", {
+      board: 3,
+      query: search,
+    })
+    setListHashtag(response.data.data.results);
+  }
   const getItem = async () => {
     setLoading(true);
     const response = await request.get("/community/posts/", {
       board: 3,
       query: search,
+      query_type: 'default',
       page: page,
     }, null);
     setList(response.data.data.results);
     setTotal(response.data.data.count);
     setLoading(false);
   }
-
+  const getItemHashtag = async (search) => {
+    setListHashtag([]);
+    setLoading(true);
+    const response = await request.get("/community/posts/", {
+      board: 3,
+      query: search,
+      query_type: 'hashtag',
+      page: page,
+    }, null);
+    setList(response.data.data.results);
+    setTotal(response.data.data.count);
+    setLoading(false);
+  }
   useEffect(() => {
-    getItem()
+    getItem();
   }, [page, search])
   return (
     <div>
@@ -87,6 +123,11 @@ export default function PromotionBoard() {
                       background="#FFFFFF"
                       color="#000000"
                     />
+                    <HashtagListWrapper>
+                      {listHashtag.map((data, index) => (
+                        <HashtagList onClick={()=>{getItemHashtag(data.name)}} key={index}>{data.name} {data.postCount}</HashtagList>
+                      ))}
+                    </HashtagListWrapper>
                   </SearchFilterBar>
                   <PromotionBoardList list={list} handleMode={handleMode} />
                   <Pagination total={total} limit="5" page={page} setPage={setPage} />
