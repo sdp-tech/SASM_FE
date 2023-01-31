b82fcd6e05cfff76112695e17a4adc7d3a298d85
YoonDH <114418121+dgfh0450@users.noreply.github.com>
2023-01-31 13:52:07 +0900

    #330 feat : search by hash searchtoggle disable
    * #330 feat : search by hash searchtoggle disable

--- src/components/Community/Contents/GroupBoard/GroupBoardList.jsx
+++ src/components/Community/Contents/GroupBoard/GroupBoardList.jsx
@@ -1,140 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
-import { useNavigate, useParams } from 'react-router-dom'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import Request from '../../../../functions/common/Request'
-import { useCookies } from 'react-cookie'
-import Loading from '../../../common/Loading'
-import GroupBoardList from './GroupBoardList'
-import GroupBoardUpload from './GroupBoardUpload'
-import SearchBar from '../../../common/SearchBar'
-import SearchBlack from '../../../../assets/img/search_black.svg'
-import Pagination from '../../../common/Pagination'
+import { Link } from 'react-router-dom'
 
-const Contents = styled.div`
+const Section = styled.div`
+
 `
-const SearchFilterBar = styled.div`
-  position: relative;
+const ListWrapper = styled.div`
+  border-top: 1px rgba(0,0,0,0.5) solid;
+`
+const List = styled.div`
+  display: flex;
+  text-align: center;
+  padding: 2% 0;
+  border-bottom: 1px rgba(0,0,0,0.5) solid;
+`
+const Title = styled.div`
   width: 60%;
-  margin: 5vh auto;
 `
-const HashtagListWrapper = styled.div`
+const Writer = styled.div`
+  width: 20%;
+`
+const CreatedAt = styled.div`
+  width: 20%;
+`
+const UploadButton = styled.div`
+  width: 10%;
   position: absolute;
-  width: 100%;
+  right: 0;
+  display: flex;
 `
-const HashtagList = styled.div`
-  background-color: rgba(0,0,0,0.5);
-  color: #FFFFFF;
-  padding: 5%;
+const StyledLink = styled(Link)`
+  width: 60%;
+  color: #000000;
+  text-decoration: none;
+  cursor: pointer;
 `
-export default function GroupBoard() {
-  const [loading, setLoading] = useState(false);
-  const [mode, setMode] = useState(false);
-  const navigate = useNavigate()
-  const [cookies, setCookie, removeCookie] = useCookies(["name"]);
-  const request = new Request(cookies, localStorage, navigate);
-  const [list, setList] = useState([]);
-  const [listHashtag, setListHashtag] = useState([]);
-  const [page, setPage] = useState(1);
-  const [total, setTotal] = useState(0);
-  const [search, setSearch] = useState('');
-  const [tempSearch, setTempSearch] = useState('');
-  const onChangeSearch = (e) => {
-    if (e.target.value[0] == '#') {
-      if (e.target.value.slice(1).length == 0) {
-        setListHashtag([]);
-      }
-      else {
-        getHashTag(e.target.value.slice(1));
-      }
-    }
-    setTempSearch(e.target.value);
-  };
-  const handleSearchToggle = async (e) => {
-    if (e) {
-      e.preventDefault();
-    }
-    if (tempSearch === null || tempSearch === "") {
-      //검색어 없을 경우 전체 리스트 반환
-      setSearch('');
-    } else {
-      //검색어 있는 경우
-      setSearch(tempSearch);
-    }
-  };
-  const handleMode = () => {
-    setMode(!mode);
-  }
-  const getHashTag = async (search) => {
-    const response = await request.get("/community/post_hashtags", {
-      board: 4,
-      query: search,
-    })
-    setListHashtag(response.data.data.results);
-  }
-  const getItem = async () => {
-    setLoading(true);
-    const response = await request.get("/community/posts/", {
-      board: 4,
-      query: search,
-      query_type: 'default',
-      page: page,
-    }, null);
-    setList(response.data.data.results);
-    setTotal(response.data.data.count);
-    setLoading(false);
-  }
-  const getItemHashtag = async (search) => {
-    setListHashtag([]);
-    setLoading(true);
-    const response = await request.get("/community/posts/", {
-      board: 4,
-      query: search,
-      query_type: 'hashtag',
-      page: page,
-    }, null);
-    setList(response.data.data.results);
-    setTotal(response.data.data.count);
-    setLoading(false);
-  }
-  useEffect(() => {
-    getItem();
-  }, [page, search])
+export default function GroupBoardList({ list, handleMode }) {
   return (
-    <div>
-      {
-        loading ?
-          <Loading />
-          :
-          <Contents>
-            {
-              mode ?
-                <GroupBoardUpload handleMode={handleMode}></GroupBoardUpload>
-                :
-                <>
-                  <SearchFilterBar>
-                    <SearchBar
-                      search={tempSearch}
-                      onChangeSearch={onChangeSearch}
-                      handleSearchToggle={handleSearchToggle}
-                      placeholder="검색어를 입력하세요"
-                      searchIcon={SearchBlack}
-                      background="#FFFFFF"
-                      color="#000000"
-                    />
-                    <HashtagListWrapper>
-                      {listHashtag.map((data, index) => (
-                        <HashtagList onClick={()=>{getItemHashtag(data.name)}} key={index}>{data.name} {data.postCount}</HashtagList>
-                      ))}
-                    </HashtagListWrapper>
-                  </SearchFilterBar>
-                  <GroupBoardList list={list} handleMode={handleMode} />
-                  <Pagination total={total} limit="5" page={page} setPage={setPage} />
-                </>
-            }
-          </Contents>
-      }
-    </div>
+    <Section>
+      <ListWrapper>
+        <List>
+          <Title>제목</Title>
+          <Writer>작성자</Writer>
+          <CreatedAt>등록일</CreatedAt>
+        </List>
+        {
+          list.map((data, index) => (
+            <List key={index}>
+              <StyledLink to={`/community/${data.id}`}>
+                {data.title}
+              </StyledLink>
+              <Writer>{data.nickname}</Writer>
+              <CreatedAt>{data.updated.slice(0, 10)}</CreatedAt>
+            </List>
+          ))
+        }
+      </ListWrapper>
+      <UploadButton
+        onClick={handleMode}
+      >
+        글쓰기
+      </UploadButton>
+    </Section>
   )
 }
