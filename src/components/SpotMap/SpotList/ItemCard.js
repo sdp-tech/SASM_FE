189f30e144e85a72f92512f580938b277fc4efda
yxnxx <jh4rgh0@gmail.com>
2023-02-03 00:49:05 +0900

    #342 feat : create google login

--- src/components/SpotMap/SpotList/ItemCard.js
+++ src/components/SpotMap/SpotList/ItemCard.js
@@ -16,7 +16,7 @@ import MarkerbgSelect from "../../../assets/img/Map/MarkerbgSelect.svg";
 const StyledCard = styled.div`
   position: relative;
   max-width: 100%;
-  margin : 0 0 0 2.5%;
+  margin: 0 0 0 2.5%;
   padding: 3%;
   border-bottom: 1px black solid;
   &:last-child {
@@ -28,22 +28,22 @@ const StyledCard = styled.div`
 `;
 
 const ImgBox = styled.div`
-  border:1px black solid;
-  min-width:15vmin;
-  min-height:15vmin;
-  max-width:15vmin;
-  max-height:15vmin;
+  border: 1px black solid;
+  min-width: 15vmin;
+  min-height: 15vmin;
+  max-width: 15vmin;
+  max-height: 15vmin;
   @media screen and (max-width: 768px) {
-    min-width:25vmin;
-    min-height:25vmin;
-    max-width:25vmin;
-    max-height:25vmin;
+    min-width: 25vmin;
+    min-height: 25vmin;
+    max-width: 25vmin;
+    max-height: 25vmin;
   }
 `;
 const TextBox = styled.div`
   width: 100%;
   display: flex;
-  flex-flow : row wrap;
+  flex-flow: row wrap;
   margin: 0 0 0 3%;
 `;
 const TitleBox = styled.div`
@@ -53,8 +53,8 @@ const TitleBox = styled.div`
   width: 100%;
   position: relative;
   display: flex;
-  justify-content:space-around;
-  flex-flow : row wrap;
+  justify-content: space-around;
+  flex-flow: row wrap;
 `;
 const ContentBox = styled.div`
   font-size: 0.75rem;
@@ -62,7 +62,7 @@ const ContentBox = styled.div`
   font-weight: 400;
   color: black;
   display: flex;
-  flex-flow : column wrap;
+  flex-flow: column wrap;
   justify-content: space-between;
 `;
 // 기존에 존재하는 버튼에 재스타일
@@ -105,15 +105,14 @@ export default function ItemCard(props) {
   useEffect(() => {
     if (categoryNum != 0) {
       setBool(true);
-    }
-    else {
+    } else {
       setBool(false);
     }
   }, [categoryNum]);
   const request = new Request(cookies, localStorage, navigate);
   const setTemp = (data) => {
     props.setTemp(data);
-  }
+  };
   // 상세보기 모달 닫기 이벤트
   const modalClose = () => {
     setModalOpen(!modalOpen);
@@ -128,7 +127,11 @@ export default function ItemCard(props) {
     if (!token) {
       alert("로그인이 필요합니다.");
     } else {
-      const response = await request.post("/places/place_like/", { id: id }, null);
+      const response = await request.post(
+        "/places/place_like/",
+        { id: id },
+        null
+      );
       // console.log("response", response);
 
       //색상 채우기
@@ -136,76 +139,54 @@ export default function ItemCard(props) {
     }
   };
   const MarkerReset = () => {
-<<<<<<< HEAD
-    const img = document.getElementById(`${id}img`);
-    const text = document.getElementById(`${id}text`);
-    if(img) {
-      img.style.transform='scale(1)';
-      if(!bool) {
-        img.setAttribute('src', MarkerDefault);
-      }
-    }
-    if(text) {
-      text.style.backgroundColor='#FFFFFF';
-      text.style.color="#000000";
-      if(!bool) {
-        text.style.display='none';
-      }
-    }
-  }
-  const MarkerChange = () => {
-    const img = document.getElementById(`${id}img`);
-    const text = document.getElementById(`${id}text`);
-    if(img) {
-      if(!bool) {
-        img.setAttribute('src', MarkerActive);
-      }
-      img.style.transform='scale(1.2)';
-    }
-    if(text) {
-      if(!bool) {
-        text.style.display='block';
-      }
-      text.style.backgroundColor='#44ADF7';
-      text.style.color="#FFFFFF";
-    }
-=======
     const text = document.getElementById(`${id}text`);
     if (text) {
-      text.style.transform='none';
+      text.style.transform = "none";
       if (!bool) {
-        text.style.display = 'none';
+        text.style.display = "none";
       }
     }
-    if(!bool){
-      document.getElementById(`${id}bg`).style.backgroundImage=`url(${MarkerbgDefault})`;
+    if (!bool) {
+      document.getElementById(
+        `${id}bg`
+      ).style.backgroundImage = `url(${MarkerbgDefault})`;
+    } else {
+      document.getElementById(
+        `${id}bg`
+      ).style.backgroundImage = `url(${MarkerbgActive})`;
     }
-    else{
-      document.getElementById(`${id}bg`).style.backgroundImage=`url(${MarkerbgActive})`;
-    }
-    document.getElementById(id).style.zIndex='1';
-  }
+    document.getElementById(id).style.zIndex = "1";
+  };
   const MarkerChange = () => {
     const text = document.getElementById(`${id}text`);
     if (text) {
-      text.style.transform='translateY(100%)';
+      text.style.transform = "translateY(100%)";
       if (!bool) {
-        text.style.display = 'block';
+        text.style.display = "block";
       }
     }
-    document.getElementById(`${id}bg`).style.backgroundImage=`url(${MarkerbgSelect})`;
-    document.getElementById(id).style.zIndex='100';
->>>>>>> 311f41b2bc39ea5b16b2e8dc335febd1fbda4dcc
-  }
+    document.getElementById(
+      `${id}bg`
+    ).style.backgroundImage = `url(${MarkerbgSelect})`;
+    document.getElementById(id).style.zIndex = "100";
+  };
 
   // 상세보기 클릭 이벤트
   const handleClick = async () => {
     setLoading(true);
     MarkerChange();
-    const response = await request.get("/places/place_detail/", { id: id }, null);
-    const response_review = await request.get("/places/place_review/", {
-      id: id,
-    }, null);
+    const response = await request.get(
+      "/places/place_detail/",
+      { id: id },
+      null
+    );
+    const response_review = await request.get(
+      "/places/place_review/",
+      {
+        id: id,
+      },
+      null
+    );
     setDetailInfo(response.data.data);
     setReviewInfo(response_review.data.data);
     setModalOpen(true);
@@ -219,7 +200,7 @@ export default function ItemCard(props) {
 
     setLoading(false);
   };
-  
+
   // params를 통해 들어왔을경우 바로 open하기
   useEffect(() => {
     if (props.modalOpen) handleClick();
@@ -256,31 +237,30 @@ export default function ItemCard(props) {
         </ImgBox>
         <TextBox>
           <TitleBox>
-            <div style={{width:'100%', cursor: "pointer"}} onClick={handleClick}>
+            <div
+              style={{ width: "100%", cursor: "pointer" }}
+              onClick={handleClick}
+            >
               {props.StoreName}
             </div>
-            <LikeButton style={{ position: 'absolute', right: '5%', bottom: '2%' }}>
+            <LikeButton
+              style={{ position: "absolute", right: "5%", bottom: "2%" }}
+            >
               {props.place_like === "ok" ? (
                 <HeartButton like={!like} onClick={toggleLike} />
               ) : (
                 <HeartButton like={like} onClick={toggleLike} />
               )}
             </LikeButton>
-            <div style={{width:'100%',fontWeight: "400", fontSize:"1rem" }}>
+            <div style={{ width: "100%", fontWeight: "400", fontSize: "1rem" }}>
               {props.StoreType}
             </div>
           </TitleBox>
 
           <ContentBox>
-            <div style={{color:"#999999"}}>
-              {props.place_review}
-            </div>
-            <div>
-              {props.Address}
-            </div>
-            <div>
-              {props.open_hours}
-            </div>
+            <div style={{ color: "#999999" }}>{props.place_review}</div>
+            <div>{props.Address}</div>
+            <div>{props.open_hours}</div>
           </ContentBox>
         </TextBox>
       </StyledCard>
