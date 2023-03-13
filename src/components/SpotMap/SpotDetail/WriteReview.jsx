6eb9c5b4b2d1ece066c324d0a689939acd26779f
YoonDH <114418121+dgfh0450@users.noreply.github.com>
2023-03-13 20:42:15 +0900

    #373 refactor : place review update keyword

--- src/components/SpotMap/SpotDetail/WriteReview.jsx
+++ src/components/SpotMap/SpotDetail/WriteReview.jsx
@@ -28,26 +28,42 @@ const FormWrapper = styled.div`
 const EachKeyWord = styled.div`
     margin:10px;
 `
-
+const PhotoBox = styled.div`
+  width: 100px;
+  height: 100px;
+  position: relative;
+`
+const DeleteButton = styled.div`
+  width: 20%;
+  height: 20%;
+  border: 1px black solid;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  right: 0;
+  top:0;
+`
 export default function WriteReview({ targetData, keywordList, id, target }) {
   const [cookies, setCookie, removeCookie] = useCookies(["name"]);
   const navigate = useNavigate();
   const request = new Request(cookies, localStorage, navigate);
   const token = localStorage.getItem('accessTK');
   const [Keyword, setKeyword] = useState([]);
+  const [photoList, setPhotoList] = useState([]);
   useEffect(() => {
     if (targetData) {
-      document.getElementById('filelist').innerHTML = null;
+      setPhotoList(targetData.photos);
       // 수정을 위해 기존에 선택한 카테고리를 보여주기
       let targetCategory = [];
       for (let i = 0; i < targetData.category.length; i++) {
         targetCategory.push(`${targetData.category[i].category}`);
-        document.getElementById(`keyword_input_${targetData.category[i].category}`).checked=true;
+        document.getElementById(`keyword_input_${targetData.category[i].category}`).checked = true;
       }
       setKeyword(targetCategory);
     }
-  }, []);
-  const fileInput = (event) => {
+  }, [targetData]);
+  const inputFile = (event) => {
     document.getElementById('filelist').innerHTML = null;
     if (event.target.files.length > 3) {
       alert('사진은 최대 3장까지 업로드 할 수 있습니다.');
@@ -72,7 +88,10 @@ export default function WriteReview({ targetData, keywordList, id, target }) {
       setKeyword(Keyword.filter((el) => el !== keyword[1]));
     }
   }
-  const reviewUpload = async (event) => {
+  const deletePhoto = (data) => {
+    setPhotoList(photoList.filter((el) => el !== data));
+  }
+  const uploadReview = async (event) => {
     if (!token) {
       alert('로그인이 필요합니다.');
       navigate('/auth');
@@ -80,25 +99,33 @@ export default function WriteReview({ targetData, keywordList, id, target }) {
     else {
       event.preventDefault();
       const formData = new FormData();
-      formData.append('place', `${id}`);
-      formData.append('contents', `${event.target.text.value}`)
+      formData.append('place', id);
+      formData.append('contents', event.target.text.value)
+      console.log('photos: ', event.target.file.files);
+      console.log('photoList: ', photoList);
       for (let i = 0; i < event.target.file.files.length; i++) {
         formData.append('photos', event.target.file.files[i]);
       }
-      formData.append('category', `${Keyword}`);
+      for(let i =0; i<photoList.length; i++) {
+        formData.append('photoList', photoList[i].imgfile);
+      }
+      formData.append('category', Array(Keyword).toString());
+      for(let item of formData) {
+        console.log(item);
+      }
       if (targetData) {
-        const response_update = await request.put(`/places/place_review/${target}/`, formData, { "Content-Type": "multipart/form-data" });
+        const response_update = await request.put(`/places/place_review/${target}/update`, formData, { "Content-Type": "multipart/form-data" });
       }
       else {
-        const response_upload = await request.post("/places/place_review/", formData, { "Content-Type": "multipart/form-data" });
+        const response_upload = await request.post("/places/place_review/create/", formData, { "Content-Type": "multipart/form-data" });
       }
-      window.location.reload();
+      //window.location.reload();
     }
   }
 
   return (
     <FormWrapper>
-      <form onSubmit={reviewUpload}>
+      <form onSubmit={uploadReview}>
         <textarea placeholder={targetData ? targetData.contents : '리뷰를 작성해주세요'} id='text' style={{ width: '99%', border: 'none' }} cols='5' autoFocus required></textarea>
         <label style={{ display: 'block' }}>키워드를 선택해주세요. (최대 3개)</label>
         <KeywordBox>
@@ -113,9 +140,20 @@ export default function WriteReview({ targetData, keywordList, id, target }) {
             })
           }
         </KeywordBox>
-        <input type="file" id="file" accept='image/*' onChange={fileInput} style={{ display: 'none' }} multiple></input>
+        <input type="file" id="file" accept='image/*' onChange={inputFile} style={{ display: 'none' }} multiple></input>
         <label htmlFor="file" style={{ display: 'block' }}>사진 업로드</label>
-        <div id="filelist"></div>
+        <div id="filelist">
+          {
+            photoList.map((data, index) => {
+              return (
+                <PhotoBox key={index}>
+                  <img src={data.imgfile} style={{ width: '100px', height: '100px' }} />
+                  <DeleteButton onClick={() => { deletePhoto(data) }}>X </DeleteButton>
+                </PhotoBox>
+              )
+            })
+          }
+        </div>
         <button type='submit' id="submit" style={{ position: 'absolute', right: '5px', bottom: '5px' }}>제출</button>
       </form>
     </FormWrapper>
