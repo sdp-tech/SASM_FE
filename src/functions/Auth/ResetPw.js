c876a1b2ba56cc3c0b572294dbf21f678468625b
Sejong Kim <sepaper@naver.com>
2023-03-31 00:58:43 +0900

    #404 fix : refactor ChangePw to ResetPw
    Signed-off-by: Sejong Kim <sepaper@naver.com>

--- src/functions/Auth/ResetPw.js
+++ src/functions/Auth/ResetPw.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-export default async function ChangePw(info) {
-  const url = process.env.REACT_APP_SASM_API_URL + "/users/pwchange/";
+export default async function ResetPw(info) {
+  const url = process.env.REACT_APP_SASM_API_URL + "/users/pw_reset/";
   if (info.code && info.password) {
     try {
-      const response = await axios.post(url, {
+      const response = await axios.put(url, {
         code: info.code,
         password: info.password,
       });
