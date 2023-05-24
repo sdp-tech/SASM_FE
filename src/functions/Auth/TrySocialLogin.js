7a48e837dde6c0936d77d40fb32dd350ce7094b3
Sejong Kim <sepaper@naver.com>
2023-05-24 13:22:58 +0900

    #369 refactor : refactor kakao social login
    Signed-off-by: Sejong Kim <sepaper@naver.com>

--- src/functions/Auth/TrySocialLogin.js
+++ src/functions/Auth/TrySocialLogin.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { KAKAO_AUTH_URL } from "./OAuth.js";
 
 axios.defaults.withCredentials = true;
 
@@ -16,7 +15,9 @@ export default async function TrySocialLogin(site) {
     window.location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${NAVER_REDIRECT_URI}&state=${NAVER_STATE}`;
 
   }
-  if (site === "kakaotalk") {
-    window.location.href = KAKAO_AUTH_URL;
+  if (site === "kakao") {
+    const KAKAO_CLIENT_ID = "7fcf4129e14411d85f4468cbffae48c0";
+    const KAKAO_REDIRECT_URI = "https://www.sasm.co.kr/auth/kakao/callback/";
+    window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
   }
 }
